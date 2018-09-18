1522fb8b57d4918572039d77f9bf77534c8d473a
Andrew Ferguson <andrewmf@umich.edu>
2018-09-18 11:36:25 -0500

    added ls.js and cat.js

--- bash.js
+++ bash.js
@@ -1,13 +1,26 @@
+const pwd = require('./pwd');
+const ls = require('./ls');
+const cat = require('./cat');
+
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
 
-  if(cmd === 'pwd'){
-    process.stdout.write(process.cwd())
+  switch (cmd) {
+    case 'ls':
+      ls();
+      break;
+    case 'pwd':
+      pwd();
+      break;
+    case 'cat':
+      cat();
+      break;
+    default:
+      process.stdout.write(`\n${cmd} is not implemented`);
+      process.stdout.write("\nprompt > ");
+      break;
   }
-  process.stdout.write('\nprompt > ');
-
-
-})
+});
 
